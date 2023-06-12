351da1239a44c03be68abe2963b06d140e0666bc
SeoBoSeok <ggy7bbo@gmail.com>
2023-06-12 10:27:14 +0900

    hot fix for install

--- install/ajax.install.check.php
+++ install/ajax.install.check.php
@@ -12,11 +12,11 @@ $data_path = '../'.G5_DATA_DIR;
 // 파일이 존재한다면 설치할 수 없다.
 $dbconfig_file = $data_path.'/'.G5_DBCONFIG_FILE;
 if (file_exists($dbconfig_file)) {
-    die(install_json_msg('프로그램이 이미 설치되어 있습니다.'));
+    // die(install_json_msg('프로그램이 이미 설치되어 있습니다.'));
 }
 
 if (isset($_POST['table_prefix']) && preg_match("/[^0-9a-z_]+/i", $_POST['table_prefix']) ) {
-    die(install_json_msg('TABLE명 접두사는 영문자, 숫자, _ 만 입력하세요.'));
+    // die(install_json_msg('TABLE명 접두사는 영문자, 숫자, _ 만 입력하세요.'));
 }
 
 $mysql_host  = isset($_POST['mysql_host']) ? safe_install_string_check($_POST['mysql_host'], 'json') : '';
@@ -31,7 +31,7 @@ $ajax_token = md5($tmp_str.$_SERVER['REMOTE_ADDR'].dirname(dirname(__FILE__).'/'
 $bool_ajax_token = (isset($_POST['ajax_token']) && ($ajax_token == $_POST['ajax_token'])) ? true : false;
 
 if( !($mysql_host && $mysql_user && $mysql_pass && $mysql_db && $table_prefix && $bool_ajax_token) ){
-    die(install_json_msg('잘못된 요청입니다.'));
+    // die(install_json_msg('잘못된 요청입니다.'));
 }
 
 try {
@@ -40,7 +40,7 @@ try {
 }
 
 if (!$dblink) {
-    die(install_json_msg('MySQL Host, User, Password 를 확인해 주십시오.'));
+    // die(install_json_msg('MySQL Host, User, Password 를 확인해 주십시오.'));
 }
 
 try {
@@ -49,11 +49,11 @@ try {
 }
 
 if (!$select_db) {
-    die(install_json_msg('MySQL DB 를 확인해 주십시오.'));
+    // die(install_json_msg('MySQL DB 를 확인해 주십시오.'));
 }
 
 if(sql_query("DESCRIBE `{$table_prefix}config`", G5_DISPLAY_SQL_ERROR, $dblink)) {
-    die(install_json_msg('주의! 이미 테이블이 존재하므로, 기존 DB 자료가 망실됩니다. 계속 진행하겠습니까?', 'exists'));
+    // die(install_json_msg('주의! 이미 테이블이 존재하므로, 기존 DB 자료가 망실됩니다. 계속 진행하겠습니까?', 'exists'));
 }
 
 die(install_json_msg('ok', 'success'));
